113019abd5b331f0e254834ae4e151b4b85d1e52
Aruna Jayaweera <it21210006@my.sliit.lk>
2024-12-06 00:22:50 +0530

    last

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+// module.exports = nextConfig
+
+
+
+
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/Aruna/' : '',
+  basePath: isProd ? '/Aruna' : '',
+  output: 'export'
+};
+
+export default nextConfig;
